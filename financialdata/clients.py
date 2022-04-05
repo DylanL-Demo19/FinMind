5beb13cd05231861c7f7b884beee54e555693289
Dylan <rex_ba@hotmail.com>
2022-04-05 19:33:17 +0800

    [create] Clients、Router 資料庫操作。

---
+++ financialdata/clients.py
@@ -0,0 +1,15 @@
+from sqlite3 import connect
+from sqlalchemy import create_engine, engine
+
+def get_mysql_financialdata_conn() -> engine.base.Connection:
+    user = "root"
+    password = "test"
+    host = "localhosta"
+    port = "3306"
+    database = "FinancialData"
+
+    address = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
+    engine = create_engine(address)
+    connect = engine.connect()
+    
+    return connect
