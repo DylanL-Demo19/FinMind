8f86b356ed6c84a735fa8a5230c55180f6ea32ca
Dylan <rex_ba@hotmail.com>
2022-04-05 20:23:57 +0800

    [update] 更改 host。

--- financialdata/clients.py
+++ financialdata/clients.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine, engine
 def get_mysql_financialdata_conn() -> engine.base.Connection:
     user = "root"
     password = "test"
-    host = "localhosta"
+    host = "localhost"
     port = "3306"
     database = "FinancialData"
 
